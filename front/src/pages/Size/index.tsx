b022b50b2af6d92cdb7fb2f9b536d6298bc56fce
Ronaldo Antonucci <ronaldo.antonuccivrb32@yahoo.com.br>
2020-08-15 16:38:19 -0300

    feat: sizes

--- front/src/pages/Size/index.tsx
+++ front/src/pages/Size/index.tsx
@@ -1,7 +1,73 @@
-import React from 'react';
+import React, {
+  useState,
+  useEffect,
+  useRef,
+  useMemo,
+  useCallback,
+} from 'react';
+import { FormHandles } from '@unform/core';
+import { Form } from '@unform/web';
+import { useHistory } from 'react-router-dom';
+
+import api from '../../services/api';
+import useOrder from '../../hooks/useOrder';
+
+import CheckboxInput from '../../components/CheckboxInput';
+
+interface Size {
+  id: string;
+  name: string;
+  description: string;
+}
+
+interface CheckboxOption {
+  id: string;
+  value: string;
+  label: string;
+}
 
 const Size: React.FC = () => {
-  return <h1>Size</h1>;
+  const formRef = useRef<FormHandles>(null);
+  const { setIngredients } = useOrder();
+  const { push } = useHistory();
+
+  const [apiSizes, setApiSizes] = useState<Size[]>([]);
+
+  useEffect(() => {
+    api.get<Size[]>('/sizes').then((response) => setApiSizes(response.data));
+  }, []);
+
+  const checkboxSizesOptions = useMemo<CheckboxOption[]>(
+    () =>
+      apiSizes.map((size) => ({
+        id: size.id,
+        value: size.name,
+        label: size.name,
+        description: size.description,
+      })),
+    [apiSizes],
+  );
+
+  const handleNext = useCallback(
+    (data) => {
+      setIngredients(data.sizes[0]);
+      push('/ingredients');
+    },
+    [push, setIngredients],
+  );
+
+  return (
+    <div>
+      <Form ref={formRef} onSubmit={handleNext}>
+        <CheckboxInput
+          name="doughs"
+          options={checkboxSizesOptions}
+          multiple={false}
+        />
+        <button type="submit">Continuar</button>
+      </Form>
+    </div>
+  );
 };
 
 export default Size;
