b4119463b2b8919d0231f49718c825ee99263571
Ronaldo Antonucci <ronaldo.antonuccivrb32@yahoo.com.br>
2020-08-15 13:07:14 -0300

    feat: checkbox component

---
+++ front/src/components/CheckboxInput/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Label = styled.label`
+  margin: 8px 0;
+  display: flex;
+  align-items: center;
+`;
